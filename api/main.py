c1512e5aab8cad66837fbaee046e80029d8405c0
goldzulu <goldzulu222@gmail.com>
2024-08-21 04:45:03 +0100

    wip

--- api/main.py
+++ api/main.py
@@ -4,10 +4,23 @@ from fastapi import FastAPI
 
 from routes import api
 from routes.admin import chat_history, data_sources, vector_stores
+from fastapi.middleware.cors import CORSMiddleware
 
 load_dotenv(".env")
 
 app = FastAPI(title="Embedchain API")
+
+# list of origins that should be allowed to access the API
+# allow any domain with metagineers in them
+origins = ["https://.*\.metagineers.com", "http://localhost", "http://localhost:3000", "http://localhost:3001","http://localhost:8000", "https://*.vercel.app","https://localhost"]
+# setup cors
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(api.router)
 app.include_router(data_sources.router)
